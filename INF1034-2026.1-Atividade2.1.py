5e1d5c36d452af79e942c721c872fe5a40f9fbe8
levyzin07 <guilhermelevy159@gmail.com>
2026-03-23 12:44:55 -0300

    Refactor turtle graphics code for clarity and functionality
    Updated turtle commands to include method calls and corrected turtle initialization.

--- INF1034-2026.1-Atividade2.1.py
+++ INF1034-2026.1-Atividade2.1.py
@@ -1,36 +1,38 @@
 from turtle import *
 
-t = Turtle
+t = Turtle()
 
 # Fazendo o plano cartesiano
 
-penup()
-goto(0,-300)
-pendown()
+t.penup()
+t.goto(0,-300)
+t.pendown()
 
 t.fd(600)
-t.stamp
+t.stamp()
 
-penup()
-goto(-300,0)
-pendown()
+t.lt(90)
+
+t.penup()
+t.goto(-300,0)
+t.pendown()
 
 t.fd(600)
-t.stamp
+t.stamp()
 
 # Fazendo as formas
 
-penup()
-goto(-100,100)
-pendown()
+t.penup()
+t.goto(-100,100)
+t.pendown()
 
 for _ in range(5):
     t.fd (100)
     t.lt (72)
 
-penup()
-goto(100,100)
-pendown()
+t.penup()
+t.goto(100,100)
+t.pendown()
 
 for _ in range(6):
     t.fd (100)
