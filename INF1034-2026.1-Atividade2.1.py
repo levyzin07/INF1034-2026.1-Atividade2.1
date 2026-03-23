909ba6a7a14545d7169ce58f7181b4892e4090be
levyzin07 <guilhermelevy159@gmail.com>
2026-03-23 13:38:22 -0300

    Update INF1034-2026.1-Atividade2.1.py

--- INF1034-2026.1-Atividade2.1.py
+++ INF1034-2026.1-Atividade2.1.py
@@ -5,7 +5,7 @@ t = Turtle()
 # Fazendo o plano cartesiano
 
 t.penup()
-t.goto(0,-300)
+t.goto(-300,0)
 t.pendown()
 
 t.fd(600)
@@ -14,7 +14,7 @@ t.stamp()
 t.lt(90)
 
 t.penup()
-t.goto(-300,0)
+t.goto(0,-300)
 t.pendown()
 
 t.fd(600)
@@ -26,16 +26,52 @@ t.penup()
 t.goto(-100,100)
 t.pendown()
 
+t.begin_fill()
+t.color("blue")
+
 for _ in range(5):
     t.fd (100)
     t.lt (72)
+
+t.end_fill()
 
 t.penup()
 t.goto(100,100)
 t.pendown()
 
+t.begin_fill()
+t.color("yellow")
+
 for _ in range(6):
     t.fd (100)
     t.rt (60)
+
+t.end_fill()
+
+t.penup()
+t.goto(-100,-200)
+t.pendown()
+
+t.begin_fill()
+t.color("red")
+
+for _ in range (8) :
+    t.fd (80)
+    t.lt (45)
+
+t.end_fill()
+
+t.penup()
+t.goto(300,-200)
+t.pendown()
+
+t.begin_fill()
+t.color("green")
+
+for _ in range (12) :
+    t.fd (60)
+    t.lt (30)
+
+t.end_fill()
 
 mainloop()
